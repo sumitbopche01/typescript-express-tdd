82a0cbf39f084445308a67fd01d6cca243986bc5
Sumit Bopche <sumit@freightbro.com>
2021-03-27 16:17:26 +0530

    first commit

---
+++ src/routes/dashboard.ts
@@ -0,0 +1,12 @@
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+// @route GET /dashboard
+// @desc Authenticate a user
+// @access PUBLIC
+router.get("/", (req: Request, res: Response) => {
+    res.send(200);
+})
+
+export default router;
---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+import routes from 'routes/index';
+
+export default function createServer() {
+    const app: Application = express();
+    //paths
+    app.get("/", (req: Request, res: Response, next: NextFunction) => {
+        res.send("Hello World");
+    })
+    app.use(routes);
+
+    return app;
+}
